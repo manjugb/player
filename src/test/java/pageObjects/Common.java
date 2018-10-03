522fce44344298aa756c8d04dbfa6ea2461a53e3
Manjunath Golla Bala <manjugbmsc@mail.com>
2018-10-03 23:13:39 +0530

    some objects were added

--- src/test/java/pageObjects/Common.java
+++ src/test/java/pageObjects/Common.java
@@ -3,6 +3,8 @@ package pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 /* @author:Manjunath Golla Bala
  * @desc:this class contains common methods 
@@ -10,46 +12,75 @@ import org.openqa.selenium.support.PageFactory;
 
 import browserFactory.DriversFactory;
 
-public class Common extends DriversFactory{
+public class Common extends DriversFactory {
 	public WebDriver driver;
-	
+
 	public Common(WebDriver driver) {
 		this.driver = driver;
-		 PageFactory.initElements(driver, this);
-	}
-	
-	//Click links text 
-	public  void clickAllHyperLinksName(String strlinkName){
-	    int numberOfElementsFound = getNumberOfElementsFound(By.linkText(strlinkName));
-	    System.out.println(numberOfElementsFound);
-	    for (int pos = 0; pos < numberOfElementsFound; pos++) {
-	        getElementWithIndex(By.tagName(strlinkName), pos).click();
-	        wd.navigate().back();
-	    }
+		PageFactory.initElements(driver, this);
 	}
 
-	
-	
-	//Click Images each one using this method 
-		public  void clickAllHyperImages(String imageName){
-		    int numberOfElementsFound = getNumberOfElementsFound(By.xpath(imageName));
-		    System.out.println(numberOfElementsFound);
-		    for (int pos = 0; pos < numberOfElementsFound; pos++) {
-		        getElementWithIndex(By.xpath(imageName), pos).click();
-		       // wd.navigate().back();
-		    }
+	// Click links text
+	public void clickAllHyperLinksName(String strlinkName) {
+		int numberOfElementsFound = getNumberOfElementsFound(By.linkText(strlinkName));
+		System.out.println(numberOfElementsFound);
+		for (int pos = 0; pos < numberOfElementsFound; pos++) {
+			getElementWithIndex(By.tagName(strlinkName), pos).click();
+			wd.navigate().back();
 		}
+	}
 
-		public  int getNumberOfElementsFound(By by) {
-		    return wd.findElements(by).size();
+	// Click Images each one using this method
+	public void clickAllHyperImages(String imageName) {
+		int numberOfElementsFound = getNumberOfElementsFound(By.xpath(imageName));
+		System.out.println(numberOfElementsFound);
+		for (int pos = 0; pos < numberOfElementsFound; pos++) {
+			getElementWithIndex(By.xpath(imageName), pos).click();
+			// wd.navigate().back();
 		}
+	}
 
-		public static WebElement getElementWithIndex(By by, int pos) {
-		    return wd.findElements(by).get(pos);
-		}
-		
-		public void waifortime() throws Throwable{
-			Thread.sleep(2000);
-		}
+	public int getNumberOfElementsFound(By by) {
+		return wd.findElements(by).size();
+	}
 
+	public static WebElement getElementWithIndex(By by, int pos) {
+		return wd.findElements(by).get(pos);
+	}
+
+	public void waifortime() throws Throwable {
+		Thread.sleep(2000);
+	}
+
+	// First Time click Play Vedio element
+	@FindBy(how = How.XPATH, using = "//button[@title='Play Video']")
+	WebElement elmFirsPlayclick;
+
+	// Click first time vedio
+	public void clk_play_first() throws Throwable {
+		elmFirsPlayclick.click();
+	}
+
+	// Find play button element from Play Chapter
+    @FindBy(how=How.XPATH,using="//div[@class='theo-player-wrapper']//div[9]//button[1]")
+    WebElement elmPlay_Pause;
+    
+    //Click Play or Pause using this method
+    public void clk_play_pause() throws Throwable{
+    	elmPlay_Pause.click();
+    }
+    
+    //Find Mute Button Element
+    @FindBy(how=How.XPATH,using="//div[contains(@class,'theo-player-wrapper')]//div[9]//button[2]")
+    WebElement elmute_unmute;
+    
+    //Click mute and mute options
+    public void clk_mute_unmute() throws Throwable{
+    	elmute_unmute.click();;
+    }
+    
+    
+    
+    
+    
 }
